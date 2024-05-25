18e51fc541e494856fc9763c71e501edd35538fc
Johnathan W <12262567+j7nw4r@users.noreply.github.com>
2024-05-25 12:24:01 -0400

    Landmarks init

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by j on 5/25/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
