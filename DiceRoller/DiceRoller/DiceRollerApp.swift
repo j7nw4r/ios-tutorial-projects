d638378c75b18919119e162e502ca52fa7994edb
Johnathan W <j7n.w4r@gmail.com>
2024-11-02 13:42:13 -0400

    Finished DiceRollerApp.

---
+++ DiceRoller/DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by j on 11/2/24.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
