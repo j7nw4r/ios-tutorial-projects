0e5793fbb17cb14611ba0b73028e2c21367401ba
Johnathan W <j7n.w4r@gmail.com>
2024-11-02 13:05:45 -0400

    Added OnboardingFlow.

---
+++ OnboardingFlow/OnboardingFlow/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  OnboardingFlow
+//
+//  Created by j on 11/2/24.
+//
+
+import SwiftUI
+
+let gradientColors: [Color] = [
+    .gradientTop,
+    .gradientBottom
+]
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            WelcomePage()
+            FeaturesPage()
+        }
+        .background(Gradient(colors: gradientColors))
+        .tabViewStyle(.page)
+        .foregroundStyle(.white)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OnboardingFlow/OnboardingFlow/OnboardingFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingFlowApp.swift
+//  OnboardingFlow
+//
+//  Created by j on 11/2/24.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
